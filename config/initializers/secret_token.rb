d7f770e092e987453fe97939d0d9d0c55d0b68e9
Don Yu <dy2212@columbia.edu>
2013-06-26 21:06:29 -0400

    creating initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lionsbase::Application.config.secret_token = 'd27e18540f6986bb74d53059f49d85fc4a506315f32708319618e92b90ad335411042732de7ed21ed763ee329036a1618022ef4a775edb02e9a86c89f90a2da5'
